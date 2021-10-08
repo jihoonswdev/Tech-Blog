6a85ea356e16cd7fb6846be0bef92faded9424cb
Jason Park <jihoonswdev@gmail.com>
2021-10-08 12:31:30 -0400

    update user route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -6,17 +6,18 @@ router.post('/', async (req, res) => {
   try {
     const newUser = await User.create({
       // TODO: SET USERNAME TO USERNAME SENT IN REQUEST
-
+      username: req.body.username,
       // TOD: SET PASSWORD TO PASSWORD SENT IN REQUEST
+      password: req.body.password
     });
 
     req.session.save(() => {
       // TODO: SET USERID userId IN REQUEST SESSION TO ID RETURNED FROM DATABASE
-
+      req.session.userId = newUser.id;
       // TODO: SET USERNAME username IN REQUEST SESSION TO USERNAME RETURNED FROM DATABASE
-
+      req.session.username = newUser.username;
       // TODO: SET LOGGEDIN loggedIn TO TRUE IN REQUEST SESSION
-
+      req.session.loggedIn = true;
       res.json(newUser);
     });
   } catch (err) {
@@ -48,11 +49,11 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       // TODO: SET USERID userId IN REQUEST SESSION TO ID RETURNED FROM DATABASE
-
+      req.session.userid = user.id;
       // TODO: SET USERNAME username IN REQUEST SESSION TO USERNAME RETURNED FROM DATABASE
-
+      req.session.username = user.username;
       // TODO: SET LOGGEDIN loggedIn TO TRUE IN REQUEST SESSION
-
+      req.session.loggedIn = true;
       res.json({ user, message: 'You are now logged in!' });
     });
   } catch (err) {
